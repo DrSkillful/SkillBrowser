f3afca46dda1416f524a6b0f02b77c0c2a833291
JohnSkill <76279774+JohnSkill@users.noreply.github.com>
2021-09-04 17:46:45 +0300

    Update SkillBrowser.py

--- SkillBrowser.py
+++ SkillBrowser.py
@@ -5,7 +5,6 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWebEngineWidgets import *
 from PyQt5.QtPrintSupport import *
-import os
 import sys
 
 # Initialising and creating the main window
